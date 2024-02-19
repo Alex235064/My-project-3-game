1572d4a5eb4b8d0628db42769fe14cdcdbd7ffe3
Alex235064 <116053389+Alex235064@users.noreply.github.com>
2024-02-19 23:06:50 +0700

    Initial commit

---
+++ Assets/scripts/EnemyAI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public List<Transform> patrolPoints;
+
+    private NavMeshAgent _navMeshAngent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InItComponentLinks();
+        PickNewPatrolposints();
+    }
+    
+    private void InItComponentLinks()
+    {
+        _navMeshAngent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PortolUpdate();
+    }
+
+    private void PortolUpdate()
+    {
+        if (_navMeshAngent.remainingDistance == 0)
+        {
+            PickNewPatrolposints();
+        }
+    }
+    private void PickNewPatrolposints()
+    {
+      _navMeshAngent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+
+}
---
+++ Assets/scripts/plaerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plaerController : MonoBehaviour
+{
+public float Gravity = 9.8f;
+    public float JampForce;
+    public float Speed;
+
+    private float _fallVelocity = 1;
+    private Vector3 _moveVector;
+
+    private CharacterController _CharacterController;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _CharacterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        _moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+        //if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && _CharacterController.isGrounded)
+        {
+          _fallVelocity = -JampForce;
+        }
+        
+    }
+    void FixedUpdate()
+    {
+        _CharacterController.Move(_moveVector * Speed * Time.fixedDeltaTime);
+
+        _fallVelocity += Gravity * Time.fixedDeltaTime;
+        _CharacterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+        if (_CharacterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
+    }
+
+}
